f63b8b6ea193456ec8d7eeb36fdfac0e7ac6fa16
Mounika Pawar <mounikapawar542@gmail.com>
2026-08-22 14:01:40 +0530

    Time: 250 ms (78.06%), Space: 0B (100%) - LeetHub

--- 0627-swap-sex-of-employees/0627-swap-sex-of-employees.sql
+++ 0627-swap-sex-of-employees/0627-swap-sex-of-employees.sql
@@ -1,5 +1,5 @@
-update Salary
-set sex = case
-    when sex = 'm' then 'f' 
-    when sex = 'f' then 'm' 
-end;   
+update salary set sex = 
+    case sex
+        when 'm' then 'f' 
+        when 'f' then 'm'
+    end;
